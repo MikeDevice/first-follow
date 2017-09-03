64036aebdab258c1907d518c628f0969b6b711bb
Mike_Device <mike.device.094@gmail.com>
2017-09-03 17:42:47 +0300

    make calling compositeDecorator strategies again

--- src/App/components/form-components/Textarea/textarea.jsx
+++ src/App/components/form-components/Textarea/textarea.jsx
@@ -27,8 +27,23 @@ export default class Textarea extends Component {
     this.onChange = this.onChange.bind(this);
   }
 
-  onChange(editorState) {
-    const newEditorState = replaceArrows(editorState);
+  onChange(_newEditorState) {
+    const { editorState } = this.state;
+    const currentContent = editorState.getCurrentContent();
+    const newContent = _newEditorState.getCurrentContent();
+
+    let newEditorState = _newEditorState;
+
+    if (currentContent !== newContent) {
+      // It's need to call 'undo' and 'redo'
+      // to call CompositeDecorator's strategies again
+      // because it's need to analyze entire text
+      // for new terminals and nonterminals
+      newEditorState = EditorState.undo(newEditorState);
+      newEditorState = EditorState.redo(newEditorState);
+
+      newEditorState = replaceArrows(newEditorState);
+    }
 
     this.setState({ editorState: newEditorState });
   }
