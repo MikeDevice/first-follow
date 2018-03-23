146d5cf35617044948bc6bfbcfbdb41eb434fd79
Mike_Device <mike.device.094@gmail.com>
2018-03-24 01:52:38 +0300

    add rules numbers

--- src/App/components/form-components/Textarea/textarea.jsx
+++ src/App/components/form-components/Textarea/textarea.jsx
@@ -32,15 +32,50 @@ export default class Textarea extends Component {
     this.setState({ editorState: newEditorState });
   }
 
+  getLineNumbers() {
+    const { editorState } = this.state;
+    const contentState = editorState.getCurrentContent();
+    const numbers = [];
+    let rulesNumber = 0;
+
+    contentState.getBlockMap().forEach((block) => {
+      const isEmpty = !block.getText().length;
+      let char;
+
+      if (!isEmpty) {
+        rulesNumber += 1;
+        char = rulesNumber;
+      }
+
+      numbers.push({
+        id: block.getKey(),
+        value: char,
+      });
+    });
+
+    return numbers;
+  }
+
   render() {
+    const lineNumbers = this.getLineNumbers();
+
     return (
       <div className="textarea">
-        <Editor
-          editorState={this.state.editorState}
-          onChange={this.onChange}
-          placeholder="Start typing your grammar..."
-          stripPastedStyles
-        />
+        <div className="textarea__numbers">
+          {lineNumbers.map(number => (
+            <div className="textarea__number" key={number.id}>
+              {number.value || '\u00A0'}
+            </div>
+          ))}
+        </div>
+        <div className="textarea__editor">
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            placeholder="Start typing your grammar..."
+            stripPastedStyles
+          />
+        </div>
       </div>
     );
   }
