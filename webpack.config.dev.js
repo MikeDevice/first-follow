eade6f4e535a9060dc37ee3bdea100f28f349f2b
Mikhail Vyrodov <me.vyrodov@ya.ru>
2019-11-30 14:25:47 +0300

    bump packages (#10)

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,25 +4,42 @@ const common = require('./webpack.config.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'eval',
+  devtool: 'cheap-eval-source-map',
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+        ],
       },
       {
         test: /\.scss$/,
         use: [
           { loader: 'style-loader' },
-          { loader: 'css-loader' },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
           {
             loader: 'sass-loader',
             options: {
-              data: '@import "variables";',
-              includePaths: [
-                path.join(__dirname, 'src/App'),
-              ],
+              sourceMap: true,
+              prependData: '@import "variables";',
+              sassOptions: {
+                includePaths: [
+                  path.join(__dirname, 'src/App'),
+                ],
+              },
             },
           },
         ],
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,63 +1,75 @@
 const path = require('path');
 const merge = require('webpack-merge');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const common = require('./webpack.config.common.js');
-const saveLicense = require('uglify-save-license');
-
-const extractTextPlugin = new ExtractTextPlugin('styles.[chunkhash].css');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const common = require('./webpack.config.common');
 
 module.exports = merge(common, {
   mode: 'production',
+  devtool: 'cheap-source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserJSPlugin({}),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: extractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader' },
-          ],
-        }),
-      },
-      {
-        test: /\.scss$/,
-        use: extractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader', options: { minimize: true } },
-            {
-              loader: 'sass-loader',
-              options: {
-                data: '@import "variables";',
+        test: /\.(sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              prependData: '@import "variables";',
+              sassOptions: {
                 includePaths: [
                   path.join(__dirname, 'src/App'),
                 ],
               },
             },
-          ],
-        }),
-        exclude: /node_modules/,
+          },
+        ],
       },
     ],
   },
   output: {
-    filename: 'bundle.[chunkhash].js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'build'),
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          output: { comments: saveLicense },
-        },
-      }),
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(['build']),
-    extractTextPlugin,
-  ],
 });
