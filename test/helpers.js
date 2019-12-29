3b11beb8bb1e0294c97fe6be2d1849e858821074
Mikhail Vyrodov <me.vyrodov@ya.ru>
2019-12-29 13:37:41 +0300

    refactor tests to es6

--- test/helpers.js
+++ test/helpers.js
@@ -1,30 +1,28 @@
-'use strict';
+const _ = require('underscore');
+const expect = require('expect.js');
 
-var _ = require('underscore'),
-	expect = require('expect.js');
+exports.checkSetHash = (expectedSetHash, setHash) => {
+  const expectedKeys = _(expectedSetHash).chain()
+    .keys()
+    .sort()
+    .value();
 
-exports.checkSetHash = function(expectedSetHash, setHash) {
-	var expectedKeys = _(expectedSetHash).chain()
-		.keys()
-		.sort()
-		.value();
+  const keys = _(setHash).chain()
+    .keys()
+    .sort()
+    .value();
 
-	var keys = _(setHash).chain()
-		.keys()
-		.sort()
-		.value();
+  expect(expectedKeys).to.eql(keys);
 
-	expect(expectedKeys).to.eql(keys);
-
-	_(expectedSetHash).each(function(right, left) {
-		expect(_(right).sort()).to.eql(_(setHash[left]).sort());
-	});
+  _(expectedSetHash).each((right, left) => {
+    expect(_(right).sort()).to.eql(_(setHash[left]).sort());
+  });
 };
 
-exports.checkPredictSets = function(expectedPredictSets, predictSets) {
-	expect(_(expectedPredictSets).size()).to.equal(_(predictSets).size());
+exports.checkPredictSets = (expectedPredictSets, predictSets) => {
+  expect(_(expectedPredictSets).size()).to.equal(_(predictSets).size());
 
-	_(expectedPredictSets).each(function(expectedSets, index) {
-		expect(_(expectedSets).sort()).to.eql(_(predictSets[index]).sort());
-	});
+  _(expectedPredictSets).each((expectedSets, index) => {
+    expect(_(expectedSets).sort()).to.eql(_(predictSets[index]).sort());
+  });
 };
