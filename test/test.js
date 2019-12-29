490020c0ced33b13d7fd5ecdb7c9bdfe0a7625cb
Mikhail Vyrodov <me.vyrodov@ya.ru>
2019-12-29 14:16:58 +0300

    refactor to es6 (#13)

--- test/test.js
+++ test/test.js
@@ -1,38 +1,36 @@
-'use strict';
+const _ = require('underscore');
+const { Grammar } = require('../index');
+const grammarFixtures = require('./fixtures').grammars;
+const helpers = require('./helpers');
 
-var Grammar = require('../index').Grammar,
-	_ = require('underscore'),
-	grammarFixtures = require('./fixtures').grammars,
-	helpers = require('./helpers');
+describe('First set', () => {
+  _(grammarFixtures).each((grammarFixture, index) => {
+    it(`Test grammar #${index + 1}`, () => {
+      const grammar = new Grammar(grammarFixture.rules);
 
-describe('First set', function() {
-	_(grammarFixtures).each(function(grammarFixture, index) {
-		it('Test grammar #' + (index + 1), function() {
-			var grammar = new Grammar(grammarFixture.rules);
-
-			helpers.checkSetHash(grammar.getFirstSets(), grammarFixture.firstSetHash);
-		});
-	});
+      helpers.checkSetHash(grammar.getFirstSets(), grammarFixture.firstSetHash);
+    });
+  });
 });
 
-describe('Follow set', function() {
-	_(grammarFixtures).each(function(grammarFixture, index) {
-		it('Test grammar #' + (index + 1), function() {
-			var grammar = new Grammar(grammarFixture.rules);
+describe('Follow set', () => {
+  _(grammarFixtures).each((grammarFixture, index) => {
+    it(`Test grammar #${index + 1}`, () => {
+      const grammar = new Grammar(grammarFixture.rules);
 
-			helpers.checkSetHash(grammar.getFollowSets(),
-				grammarFixture.followSetHash);
-		});
-	});
+      helpers.checkSetHash(grammar.getFollowSets(),
+        grammarFixture.followSetHash);
+    });
+  });
 });
 
-describe('Predict set', function() {
-	_(grammarFixtures).each(function(grammarFixture, index) {
-		it('Test grammar #' + (index + 1), function() {
-			var grammar = new Grammar(grammarFixture.rules);
+describe('Predict set', () => {
+  _(grammarFixtures).each((grammarFixture, index) => {
+    it(`Test grammar #${index + 1}`, () => {
+      const grammar = new Grammar(grammarFixture.rules);
 
-			helpers.checkPredictSets(grammar.getPredictSets(),
-				grammarFixture.predictSets);
-		});
-	});
+      helpers.checkPredictSets(grammar.getPredictSets(),
+        grammarFixture.predictSets);
+    });
+  });
 });
