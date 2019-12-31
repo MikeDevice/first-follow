f122041f89ca1479a7d1a1e4d9f541be7deead76
Mikhail Vyrodov <me.vyrodov@ya.ru>
2019-12-31 15:00:34 +0300

    refactor tests and remove underscore from dependencies

--- test/helpers.js
+++ test/helpers.js
@@ -1,28 +1,12 @@
-const _ = require('underscore');
 const expect = require('expect.js');
 
 exports.checkSetHash = (expectedSetHash, setHash) => {
-  const expectedKeys = _(expectedSetHash).chain()
-    .keys()
-    .sort()
-    .value();
-
-  const keys = _(setHash).chain()
-    .keys()
-    .sort()
-    .value();
+  const expectedKeys = Object.keys(expectedSetHash).sort();
+  const keys = Object.keys(setHash).sort();
 
   expect(expectedKeys).to.eql(keys);
 
-  _(expectedSetHash).each((right, left) => {
-    expect(_(right).sort()).to.eql(_(setHash[left]).sort());
-  });
-};
-
-exports.checkPredictSets = (expectedPredictSets, predictSets) => {
-  expect(_(expectedPredictSets).size()).to.equal(_(predictSets).size());
-
-  _(expectedPredictSets).each((expectedSets, index) => {
-    expect(_(expectedSets).sort()).to.eql(_(predictSets[index]).sort());
+  Object.entries(expectedSetHash).forEach(([left, right]) => {
+    expect(right.sort()).to.eql(setHash[left].sort());
   });
 };
--- test/test.js
+++ test/test.js
@@ -1,36 +1,33 @@
-const _ = require('underscore');
 const { Grammar } = require('../index');
-const grammarFixtures = require('./fixtures').grammars;
-const helpers = require('./helpers');
+const { grammars } = require('./fixtures');
+const { checkSetHash } = require('./helpers');
 
 describe('First set', () => {
-  _(grammarFixtures).each((grammarFixture, index) => {
+  grammars.forEach((grammarFixture, index) => {
     it(`Test grammar #${index + 1}`, () => {
       const grammar = new Grammar(grammarFixture.rules);
 
-      helpers.checkSetHash(grammar.getFirstSets(), grammarFixture.firstSetHash);
+      checkSetHash(grammar.getFirstSets(), grammarFixture.firstSetHash);
     });
   });
 });
 
 describe('Follow set', () => {
-  _(grammarFixtures).each((grammarFixture, index) => {
+  grammars.forEach((grammarFixture, index) => {
     it(`Test grammar #${index + 1}`, () => {
       const grammar = new Grammar(grammarFixture.rules);
 
-      helpers.checkSetHash(grammar.getFollowSets(),
-        grammarFixture.followSetHash);
+      checkSetHash(grammar.getFollowSets(), grammarFixture.followSetHash);
     });
   });
 });
 
 describe('Predict set', () => {
-  _(grammarFixtures).each((grammarFixture, index) => {
+  grammars.forEach((grammarFixture, index) => {
     it(`Test grammar #${index + 1}`, () => {
       const grammar = new Grammar(grammarFixture.rules);
 
-      helpers.checkPredictSets(grammar.getPredictSets(),
-        grammarFixture.predictSets);
+      checkSetHash(grammar.getPredictSets(), grammarFixture.predictSets);
     });
   });
 });
