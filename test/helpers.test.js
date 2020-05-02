b08646cf7a0420ee623fd0ac8b3666f806d28e62
Mikhail Vyrodov <me.vyrodov@ya.ru>
2020-05-02 13:45:06 +0300

    refactor (#24)
    * refactor helpers
    
    * refactor tests
    
    * refactor Grammar
    
    * change size

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -1,60 +1,57 @@
-const { EMPTY_CHAIN } = require('../lib/constants');
-const { addToSet, mapSetsToArrays, indexBy } = require('../lib/helpers');
+const { compact, indexBy, union, uniq } = require('../lib/helpers');
 
-describe('Tests for "addToSet" function', () => {
-  const a = new Set([1, 2, 3]);
-  const b = new Set([4, 5, EMPTY_CHAIN]);
+describe('Tests for "compact" function', () => {
+  const arr = [1, 'a', ['1'], { a: 'b' }];
 
-  test('Without options', () => {
-    const result = addToSet(a, b);
-
-    expect(result).toEqual(new Set([...a, ...b]));
+  test('Call function without falsy array values', () => {
+    expect(compact(arr)).toEqual(arr);
   });
 
-  test('With "skipEmptyChain" option', () => {
-    const result = addToSet(a, b, { skipEmptyChain: true });
-    const set = new Set([...a, ...b]);
-    set.delete(EMPTY_CHAIN);
+  test('Call function with falsy array values', () => {
+    const arrWithFalsyValues = arr.concat([null, NaN, undefined, 0, '']);
 
-    expect(result).toEqual(set);
-  });
-
-  test('With "skipEmptyChain" option without empty chain in second argument', () => {
-    const result = addToSet(b, a, { skipEmptyChain: true });
-
-    expect(result).toEqual(new Set([...a, ...b]));
-  });
-});
-
-describe('Tests for "mapSetsToArrays" function', () => {
-  const sets = {
-    A: new Set([1, 2]),
-    B: new Set([3]),
-  };
-
-  test('Call function', () => {
-    const result = mapSetsToArrays(sets);
-
-    expect(Object.keys(result)).toEqual(Object.keys(sets));
-    expect(Object.values(result)).toEqual(Object.values(sets).map((arr) => [...arr]));
+    expect(compact(arrWithFalsyValues)).toEqual(arr);
   });
 });
 
 describe('Tests for "indexBy" function', () => {
   const arr = ['a', 'b', 'c'];
 
-  test('Without "iteratee" function', () => {
+  test('Call function', () => {
     const result = indexBy(arr);
 
     expect(Object.keys(result)).toEqual(arr);
     expect(Object.values(result)).toEqual(arr);
   });
+});
 
-  test('With "iteratee" function', () => {
-    const iteratee = (item, index) => item.toUpperCase() + index;
-    const result = indexBy(arr, iteratee);
+describe('Tests for "union" function', () => {
+  const arr1 = [1, 2, 3];
+  const arr2 = ['a', 'b', 'c'];
 
-    expect(Object.keys(result)).toEqual(arr.map(iteratee));
-    expect(Object.values(result)).toEqual(arr);
+  test('Call function with the same arrays', () => {
+    expect(union(arr1, arr1)).toEqual(arr1);
+  });
+
+  test('Call function with different arrays', () => {
+    expect(union(arr1, arr2)).toEqual([...arr1, ...arr2]);
+  });
+
+  test('Call function with mixed array values', () => {
+    expect(union([...arr1, arr2[0]], [...arr2, ...arr1])).toEqual([...arr1, ...arr2]);
+  });
+});
+
+describe('Tests for "uniq" function', () => {
+  const arr = [1, 'a', 0, null];
+
+  test('Call function without identical values', () => {
+    expect(uniq(arr)).toEqual(arr);
+  });
+
+  test('Call function with identical values', () => {
+    const arrWithIdenticalValues = arr.concat(arr);
+
+    expect(uniq(arrWithIdenticalValues)).toEqual(arr);
   });
 });
