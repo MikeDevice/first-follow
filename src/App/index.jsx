3962dc9d9ee80858f9543c0b994d698e4b9e4120
Mikhail Vyrodov <me.vyrodov@ya.ru>
2020-07-17 22:50:33 +0300

    improve object-curly-spacing eslint rule

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { hot } from 'react-hot-loader/root';
+import {hot} from 'react-hot-loader/root';
 import './index.scss';
 
 const App = () => <div>Hello World!</div>;
