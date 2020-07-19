088ac0815b80f1992b6364308efbd3e1cf583ca4
Mikhail Vyrodov <me.vyrodov@ya.ru>
2020-07-19 15:51:21 +0300

    add some styles

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import {hot} from 'react-hot-loader/root';
 import './index.scss';
 
-const App = () => <div>Hello World!</div>;
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>First Follow</h1>
+        <p>A small tool for calculating first, follow and predict sets for the grammar</p>
+      </header>
+      <main>
+        <section>Editor</section>
+        <section>First sets</section>
+        <section>Follow sets</section>
+        <section>Predict sets</section>
+      </main>
+    </div>
+  );
+}
 
 export default hot(App);
